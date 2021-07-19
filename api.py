2e1e9899514358c8722b28035aed65041ec6d066
Peter Kaminski <kaminski@istori.com>
2021-07-19 12:33:06 -0700

    more functions. the get_clean functions don't quite work correctly yet

--- api.py
+++ api.py
@@ -9,6 +9,8 @@
 VERSION = 'v0.0.1'
 APPNAME = 'ZoomChatter'
 
+import re
+
 # Flask and Flask-RESTful
 from flask import Flask, request
 from flask_restful import reqparse, abort, Api, Resource
@@ -19,9 +21,35 @@ from urlextract import URLExtract
 app = Flask(__name__)
 api = Api(app)
 
+# links
+# participants
+# clean
+# clean_grouped
+# clean_no_names
+
+# ?output=
+# ?flags=include_private
+# ?format=html (text (utf-8), ascii, markdown, HTML, JSON)
+
+def get_participants(input):
+    participants = re.findall(r"^\d\d:\d\d:\d\d\t([^\t]+):\t", input, re.MULTILINE)
+    return list(set(participants))
+
 def get_links(input):
     extractor = URLExtract()
-    return extractor.find_urls(input)
+    urls = extractor.find_urls(input)
+    return list(set(urls))
+
+def get_clean(input):
+    names_and_lines = re.findall(r"^\d\d:\d\d:\d\d\t([^\t]+):\t([^\n\r]*)", input, re.MULTILINE)
+    return names_and_lines
+
+def get_clean_no_names(input):
+    lines = re.findall(r"^\d\d:\d\d:\d\d\t[^\t]+:\t([^\n\r]*)", input, re.MULTILINE)
+    return "\n\n".join(lines)
+
+def get_lines(input):
+    return input.splitlines()
 
 class ZoomChatter(Resource):
     def post(self):
@@ -29,9 +57,18 @@ class ZoomChatter(Resource):
         input = ''
         for key, file in request.files.items(multi=True):
             input += file.read().decode("utf-8")
+        # change different possible linebreak characters to \n
+        re.sub(r"[\n\r]|\u2028", "\n", input)
+
+        # return data
         return {
-            'length': len(input),
+            'input_length': len(input),
+            'participants': get_participants(input),
             'links': get_links(input),
+#            'clean': get_clean(input),
+#            'clean_no_names': get_clean_no_names(input),
+            'lines': get_lines(input),
+            'input_linebreaks_fixed': input,
         }
 
 api.add_resource(ZoomChatter, '/zoomchatter')
